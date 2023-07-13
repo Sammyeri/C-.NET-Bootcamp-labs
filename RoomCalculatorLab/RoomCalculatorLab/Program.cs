0fa245297f266b202449d38b41a4439712e27827
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2023-07-13 13:41:26 -0400

    psuedo code for room analyzzer lab

---
+++ RoomCalculatorLab/RoomCalculatorLab/Program.cs
@@ -0,0 +1,8 @@
+﻿
+//Room Calculator Lab
+
+//prompts the user for length and width of the classroom
+
+//displays the area and perimeter of that classroom
+
+//classifies room as small(<=250 sqft), medium (250 - 650sqt), or large( >= 650sqft)
