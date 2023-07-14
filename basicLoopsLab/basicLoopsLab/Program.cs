73c455adff3567660de799a2e2c5ec039330757d
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2023-07-13 21:57:36 -0400

    part 2 complete

--- basicLoopsLab/basicLoopsLab/Program.cs
+++ basicLoopsLab/basicLoopsLab/Program.cs
@@ -29,13 +29,60 @@ do
 
 
 //part 2
-//prompt user for number,
+isRunning = true;
+while (isRunning)
+{
+    //prompt user for number,
+    Console.Write("Enter a positive whole number: ");
+    string userInputpt2 = Console.ReadLine();
+    int userNumberpt2;
+
+    bool isSuccessfulpt2 = int.TryParse(userInputpt2, out userNumberpt2);
+
+    if (!isSuccessfulpt2 || userNumberpt2 < 0)
+    {
+        Console.WriteLine("Sorry " + userInputpt2 + " is not a valid positivce whole number number, please try again");
+        continue;
+    }
 
 //for loop to output all from user number to 0,
+    for(int i = 0; i <= userNumberpt2; i++)
+    {
+        Console.Write(userNumberpt2 - i + " ");
+        
+    }
+    
+    Console.WriteLine("");
+    
+    //for loop from 0 to user number
+    for (int i = 0; i <= userNumberpt2; i++)
+    {
+        Console.Write(i + " ");
 
-//for loop from 0 to user number
+    }
 
-//ask if they would like to continue
+    Console.WriteLine("");
+
+    //ask if they would like to continue
+    Console.WriteLine("Would you like to continue? (y/n)");
+    ConsoleKeyInfo userInputchoicept2 = Console.ReadKey();
+    Console.WriteLine("");
+
+    if (userInputchoicept2.KeyChar == 'y')
+    {
+        isRunning = true;
+    }
+    else if (userInputchoicept2.KeyChar == 'n')
+    {
+        isRunning = false;
+    }
+    else
+    {
+        Console.WriteLine("Sorry, " + userInputchoicept2.KeyChar + " is not a valid response, please try again");
+
+    }
+
+}
 
 
 //part 3 and 4
