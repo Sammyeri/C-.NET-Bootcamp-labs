3d622d112b38dee60ffd36492f5d3d6ac2117ecf
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2023-07-17 23:57:37 -0400

    asks user if they want to play again

--- Lab3-DiceRollerLab/Lab3-DiceRollerLab/Program.cs
+++ Lab3-DiceRollerLab/Lab3-DiceRollerLab/Program.cs
@@ -4,67 +4,91 @@
 //ask user to enter dice size, make sure to only take positive whole numbers
 using Lab3_DiceRollerLab;
 
-bool passed = false;
+bool continuing = true;
 
-int diceSize = 0;
-while (!passed)
+while (continuing)
 {
-    Console.WriteLine("What size of dice would you like to roll today?");
-    string userInputSize = Console.ReadLine();
+    bool passed = false;
 
-    passed = int.TryParse(userInputSize, out diceSize);
-
-    if (passed && diceSize > 0)
+    int diceSize = 0;
+    while (!passed)
     {
-        break;
+        Console.WriteLine("What size of dice would you like to roll today?");
+        string userInputSize = Console.ReadLine();
+
+        passed = int.TryParse(userInputSize, out diceSize);
+
+        if (passed && diceSize > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine($"Sorry, {userInputSize} is not a vaild dice size. ");
+            passed = false;
+            continue;
+        }
     }
-    else
+    //prompt user to roll the dice
+    bool readyRoll = false;
+    while (!readyRoll)
     {
-        Console.WriteLine($"Sorry, {userInputSize} is not a vaild dice size. ");
-        passed = false;
-        continue;
+        Console.WriteLine("Roll the dice? (y/n)");
+        ConsoleKeyInfo userReady = Console.ReadKey();
+        Console.WriteLine(" ");
+
+        if (userReady.KeyChar == 'y')
+        {
+            break;
+        }
+        else if (userReady.KeyChar == 'n')
+        {
+            continue;
+        }
+        else
+        {
+            Console.WriteLine($"Sorry, {userReady.KeyChar} is not a valid choice.");
+        }
+    }
+    //roll 2 dice
+    int dice1 = Method.roll(diceSize);
+    int dice2 = Method.roll(diceSize);
+
+    //display results of each and total
+    int total = Method.total(dice1, dice2);
+
+    Console.WriteLine($"Dice 1: {dice1}");
+    Console.WriteLine($"Dice 2: {dice2}");
+    Console.WriteLine($"Total: {total}");
+    //for 6 sided only put extra messages
+    if (diceSize == 6)
+    {
+        Method.message(dice1, dice2);
+        Method.extraMessage(dice1, dice2);
+    }
+
+
+    //asks if user wants to roll again
+
+    while (continuing)
+    {
+        Console.WriteLine("Would you like to play again (y/n)");
+        ConsoleKeyInfo wantContinue = Console.ReadKey();
+        Console.WriteLine(" ");
+
+        if (wantContinue.KeyChar == 'y')
+        {
+            continuing = true;
+            break;
+        }
+        else if (wantContinue.KeyChar == 'n')
+        {
+            continuing = false;
+            break;
+        }
+        else
+        {
+            Console.WriteLine($"Sorry {wantContinue.KeyChar} is not a valid choice.");
+        }
     }
 }
-//prompt user to roll the dice
-bool readyRoll = false;
-while (!readyRoll) 
-{
-    Console.WriteLine("Roll the dice? (y/n)");
-    ConsoleKeyInfo userReady = Console.ReadKey();
-    Console.WriteLine(" ");
-
-    if(userReady.KeyChar == 'y')
-    {
-        break;
-    }
-    else if(userReady.KeyChar == 'n')
-    {
-        continue;
-    }
-    else
-    {
-        Console.WriteLine($"Sorry, {userReady.KeyChar} is not a valid choice.");
-    }
-}
-//roll 2 dice
-int dice1 = Method.roll(diceSize);
-int dice2 = Method.roll(diceSize);
-
-dice1 =  6;
-dice2 = 6;
-
-//display results of each and total
-int total = Method.total(dice1, dice2);
-
-Console.WriteLine($"Dice 1: {dice1}");
-Console.WriteLine($"Dice 2: {dice2}");
-Console.WriteLine($"Total: {total}");
-//for 6 sided only put extra messages
-if(diceSize == 6)
-{
-    Method.message(dice1, dice2);
-    Method.extraMessage(dice1, dice2);
-}
-
-
-//asks if user wants to roll again
