018949a4f5adc71f93e39f71913f9c8772b7c710
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2023-07-18 00:25:39 -0400

    now calculates and displays result for user

--- Basic-For-Loops-Lab/Basic-For-Loops-Lab/Program.cs
+++ Basic-For-Loops-Lab/Basic-For-Loops-Lab/Program.cs
@@ -2,8 +2,37 @@
 //Basic For-loops Lab
 
 //prompt user to enter a number
+
 Console.WriteLine("Please input a whole number.");
 string userInput = Console.ReadLine();
 
-Console.WriteLine(userInput);
+int userNumber = 0;
+bool passed = int.TryParse(userInput, out userNumber);
+
+int total = 0;
+
+if (passed)
+{
+    if(userNumber > 0)
+    {
+        for(int i = 0; i < userNumber; i++)
+        {
+            total += i;
+        }
+        Console.WriteLine(total);
+    }
+    else
+    {
+        for(int j = 0; j > userNumber; j--)
+        {
+            total += j;
+        }
+        Console.WriteLine(total);
+    }
+}
+else
+{
+    Console.WriteLine($"Sorry {userInput} is not a valid whole number");
+    
+}
 //use for loop to calculate the sum of all the numbers from 1 to the chosen number
