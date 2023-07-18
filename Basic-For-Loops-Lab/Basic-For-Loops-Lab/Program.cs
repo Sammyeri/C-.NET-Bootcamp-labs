73522553d70d5d61cdb42c9f2d37652a97d0341f
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2023-07-18 00:16:40 -0400

    prompts user for a whole number

--- Basic-For-Loops-Lab/Basic-For-Loops-Lab/Program.cs
+++ Basic-For-Loops-Lab/Basic-For-Loops-Lab/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Basic For-loops Lab
+
+//prompt user to enter a number
+Console.WriteLine("Please input a whole number.");
+string userInput = Console.ReadLine();
+
+Console.WriteLine(userInput);
+//use for loop to calculate the sum of all the numbers from 1 to the chosen number
