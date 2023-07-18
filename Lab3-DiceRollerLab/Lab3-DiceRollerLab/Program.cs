4321d65d2cdbc04063f2658e5c5cee6d02f2dec6
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2023-07-17 21:23:42 -0400

    Psuedo code completed

---
+++ Lab3-DiceRollerLab/Lab3-DiceRollerLab/Program.cs
@@ -0,0 +1,24 @@
+﻿
+//Dice Roller Lab
+
+//ask user to enter dice size, make sure to only take positive whole numbers
+
+//prompt user to roll the dice
+
+//roll 2 dice
+
+//display results of each and total
+
+//for 6 sided only put extra messages
+
+//snake eyes, 2 ones
+
+//ace duece: 1 and 2
+
+//Box cars: 2 6s
+
+//win: total of 7 or 11
+
+//craps: total of 2, 3, 12
+
+//asks if user wants to roll again
