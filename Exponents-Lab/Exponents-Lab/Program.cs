a31e2467d1f6b5526dea079282a5e3f06d759c2a
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2023-07-19 16:36:51 -0400

    now squares and cubes number displaying all integers up to the given number

--- Exponents-Lab/Exponents-Lab/Program.cs
+++ Exponents-Lab/Exponents-Lab/Program.cs
@@ -2,6 +2,7 @@
 //Exponents lab
 
 bool valid = false;
+int userInput = 0;
 
 while (!valid)
 {
@@ -9,7 +10,6 @@ while (!valid)
     Console.WriteLine("Enter a positive integer: ");
     string userInputRaw = Console.ReadLine();
 
-    int userInput;
     //only accepts positive, whole numbers as input, and maiximum number input is the largest that can still fit in int when cubed
     bool succeded = int.TryParse(userInputRaw, out userInput);
     if (succeded)
@@ -34,6 +34,14 @@ while (!valid)
 }
 
 //displays table of squares and budes from 1 to value entered
+for (int i = 1; i <= userInput; i++)
+{
+    int square = i * i;
+    int cube = i * i * i;
 
+    Console.WriteLine($"Number: {i}");
+    Console.WriteLine($"Squared: {square}");
+    Console.WriteLine($"Cubed: {cube}");
+}
 
 //prompts user to continue
