8a8bbb158a1b49389de258eadd22a3cbfa663d57
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2023-07-19 16:19:45 -0400

    now accepts user input

--- Exponents-Lab/Exponents-Lab/Program.cs
+++ Exponents-Lab/Exponents-Lab/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Exponents lab
+
+//prompts user to enter an integer
+Console.WriteLine("Enter an integer: ");
+string userInputRaw = Console.ReadLine();
+
+Console.WriteLine(userInputRaw);
+//only accepts positive, >0, and whole numbers as input, and maiximum number input is the largest that can still fit in int when cubed
+
+//displays table of squares and budes from 1 to value entered
+
+
+//prompts user to continue
