5bbb0f34551d4ff238cb460e59a1b961e1b958ee
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2023-07-19 16:46:19 -0400

    now prompts user to continue

--- Exponents-Lab/Exponents-Lab/Program.cs
+++ Exponents-Lab/Exponents-Lab/Program.cs
@@ -1,47 +1,75 @@
 ﻿
 //Exponents lab
-
-bool valid = false;
-int userInput = 0;
-
-while (!valid)
+bool again = true;
+while (again)
 {
-    //prompts user to enter an integer
-    Console.WriteLine("Enter a positive integer: ");
-    string userInputRaw = Console.ReadLine();
 
-    //only accepts positive, whole numbers as input, and maiximum number input is the largest that can still fit in int when cubed
-    bool succeded = int.TryParse(userInputRaw, out userInput);
-    if (succeded)
+
+    bool valid = false;
+    int userInput = 0;
+
+    while (!valid)
     {
-        if(userInput > 1290)
+        //prompts user to enter an integer
+        Console.WriteLine("Enter a positive integer: ");
+        string userInputRaw = Console.ReadLine();
+
+        //only accepts positive, whole numbers as input, and maiximum number input is the largest that can still fit in int when cubed
+        bool succeded = int.TryParse(userInputRaw, out userInput);
+        if (succeded)
         {
-            Console.WriteLine($"Sorry {userInput} is too large to square and cube.");
-        }
-        else if(userInput > 0)
-        {
-            valid = true;
+            if (userInput > 1290)
+            {
+                Console.WriteLine($"Sorry {userInput} is too large to square and cube.");
+            }
+            else if (userInput > 0)
+            {
+                valid = true;
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, {userInput} is not a positive integer.");
+            }
         }
         else
         {
-            Console.WriteLine($"Sorry, {userInput} is not a positive integer.");
+            Console.WriteLine($"Sorry, {userInputRaw} is not a valid integer.");
         }
     }
-    else
+
+    //displays table of squares and budes from 1 to value entered
+    for (int i = 1; i <= userInput; i++)
     {
-        Console.WriteLine($"Sorry, {userInputRaw} is not a valid integer.");
+        int square = i * i;
+        int cube = i * i * i;
+
+        Console.WriteLine($"Number: {i}");
+        Console.WriteLine($"Squared: {square}");
+        Console.WriteLine($"Cubed: {cube}");
     }
+
+    bool incorrect = true;
+    while (incorrect)
+    {
+        //prompts user to continue
+        Console.WriteLine("Continue? (y/n)");
+        ConsoleKeyInfo choice = Console.ReadKey();
+        Console.WriteLine(" ");
+
+        if (choice.KeyChar == 'y')
+        {
+            break;
+        }
+        else if (choice.KeyChar == 'n')
+        {
+            again = false;
+            break;
+        }
+        else
+        {
+            Console.WriteLine($"Sorry, {choice.KeyChar} is not a valid input");
+        }
+
+    }
+
 }
-
-//displays table of squares and budes from 1 to value entered
-for (int i = 1; i <= userInput; i++)
-{
-    int square = i * i;
-    int cube = i * i * i;
-
-    Console.WriteLine($"Number: {i}");
-    Console.WriteLine($"Squared: {square}");
-    Console.WriteLine($"Cubed: {cube}");
-}
-
-//prompts user to continue
