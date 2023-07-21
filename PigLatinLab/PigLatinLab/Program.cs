f694b5266b06604cab3314fe1695807545ab010d
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2023-07-21 10:21:38 -0400

    asks user if they want to translate another line

--- PigLatinLab/PigLatinLab/Program.cs
+++ PigLatinLab/PigLatinLab/Program.cs
@@ -1,36 +1,88 @@
 ﻿
 //Pig latin lab
 
-bool valid = false;
-while (!valid)
-{
-    //prompt user for a word
-    Console.WriteLine("Please enter a word you would like translated.");
-    string word = Console.ReadLine();
 
-    if (word != "")
+bool again = false;
+
+while (!again)
+{
+
+    string word = "";
+    bool valid = false;
+
+    while (!valid)
     {
-        for (int i = 0; i < word.Length; i++)
+        //prompt user for a word
+        Console.WriteLine("Please enter a word you would like translated.");
+        word = Console.ReadLine();
+
+        if (word != "")
         {
-            if (word[i] != ' ')
+            for (int i = 0; i < word.Length; i++)
             {
-                valid = true;
+                if (word[i] != ' ')
+                {
+                    valid = true;
+                }
+            }
+            if (!valid)
+            {
+                Console.WriteLine($"Sorry, {word} is not a valid word");
             }
         }
-        if (!valid)
+        else
         {
             Console.WriteLine($"Sorry, {word} is not a valid word");
         }
     }
+    word = word.Trim();
+
+    //translates text to pig latin
+
+    if (word.ToLower()[0] == 'a' || word.ToLower()[0] == 'e' || word.ToLower()[0] == 'i' || word.ToLower()[0] == 'o' || word.ToLower()[0] == 'u')
+    {
+        Console.WriteLine($"{word}way");
+    }
     else
     {
-        Console.WriteLine($"Sorry, {word} is not a valid word");
+        int count = 0;
+        string temp = "";
+        for (int i = 0; i < word.Length; i++)
+        {
+            if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u')
+            {
+                break;
+            }
+            else
+            {
+                temp = temp + word[i];
+                count++;
+            }
+        }
+        Console.WriteLine(word.Substring(count) + temp + "ay");
+    }
+
+    //displays result
+
+    //ask if want to translate another word
+    while (true)
+    {
+        Console.WriteLine("Translate another line? (y/n) ");
+        ConsoleKeyInfo choice = Console.ReadKey();
+        Console.WriteLine(" ");
+
+        if(choice.KeyChar == 'y')
+        {
+            break;
+        }
+        else if (choice.KeyChar == 'n')
+        {
+            again = true;
+            break;
+        }
+        else
+        {
+            Console.WriteLine($"Sorry, {choice.KeyChar} is not a valid choice. ");
+        }
     }
 }
-
-
-//translates text to pig latin
-
-//displays result
-
-//ask if want to translate another word
