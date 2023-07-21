ec0380a54d8bbb785361e66d9ed286ac1dce8b33
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2023-07-20 21:44:15 -0400

    gets and checks user input for valid input

--- PigLatinLab/PigLatinLab/Program.cs
+++ PigLatinLab/PigLatinLab/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Pig latin lab
+
+bool valid = false;
+while (!valid)
+{
+    //prompt user for a word
+    Console.WriteLine("Please enter a word you would like translated.");
+    string word = Console.ReadLine();
+
+    if (word != "")
+    {
+        for (int i = 0; i < word.Length; i++)
+        {
+            if (word[i] != ' ')
+            {
+                valid = true;
+            }
+        }
+        if (!valid)
+        {
+            Console.WriteLine($"Sorry, {word} is not a valid word");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Sorry, {word} is not a valid word");
+    }
+}
+
+
+//translates text to pig latin
+
+//displays result
+
+//ask if want to translate another word
