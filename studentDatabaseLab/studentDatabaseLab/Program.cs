5f6c52d4b1da1850ace00c3d3da8fe230d65eb26
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2023-07-26 18:14:34 -0400

    arrays added

--- studentDatabaseLab/studentDatabaseLab/Program.cs
+++ studentDatabaseLab/studentDatabaseLab/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Student database lab
+
+//create name array, filled with students names
+
+string[] studentNames = new string[5] { "Jim", "Luna", "Jenny", "Carl", "Kat" };
+
+//create hometown array, filled with students hometown at same index as the students name
+
+string[] studentHometowns = new string[5] { "Austin, Texas", "Paris, France", "Lansing, Michigan", "Tokyo, Japan", "St. Paul, Minnesota" };
+
+//create favorite food array, filled with student favorite foods
+
+string[] studentFavoriteFoods = new string[5] { "Steak", "Gelato", "Pizza", "Roast Duck", "Chicken and Rice" };
+
+
+//prompt user to asl about a particular student by number
+
+//convert user input to integer
+
+//find and print student's name
+
+//ask user which category to display, hometown or favorite food
+
+//display relevant info
+
+//ask if user would like to learn about another student
