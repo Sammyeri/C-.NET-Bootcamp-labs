7fabbb6f95b522c016ec6ddbcacc47b8519ae7c2
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2023-07-26 18:28:46 -0400

    validates number and displays result

--- studentDatabaseLab/studentDatabaseLab/Program.cs
+++ studentDatabaseLab/studentDatabaseLab/Program.cs
@@ -13,16 +13,38 @@ string[] studentHometowns = new string[5] { "Austin, Texas", "Paris, France", "L
 
 string[] studentFavoriteFoods = new string[5] { "Steak", "Gelato", "Pizza", "Roast Duck", "Chicken and Rice" };
 
+Console.WriteLine("Welcome!");
 
 //prompt user to asl about a particular student by number
+while (true)
+{
 
-Console.WriteLine($"Welcome! Which student would you like to learn more about? Enter a number 1-{studentNames.Length}");
-string userChoice = Console.ReadLine();
 
-//convert user input to integer
+    Console.WriteLine($"Which student would you like to learn more about? Enter a number 1-{studentNames.Length}");
+    string userChoiceRaw = Console.ReadLine();
 
-//find and print student's name
+    //convert user input to integer
+    int userChoice = 0;
+    bool succeded = int.TryParse(userChoiceRaw, out userChoice);
 
+    //find and print student's name
+
+    if (succeded && userChoice > 0 && userChoice <= studentNames.Length)
+    {
+        Console.WriteLine($"Student {userChoice} is {studentNames[userChoice - 1]}.");
+        break;
+    }
+    else if (succeded)
+    {
+        Console.WriteLine($"Sorry, {userChoice} is not in the range provided.");
+        continue;
+    }
+    else
+    {
+        Console.WriteLine($"Sorry, {userChoiceRaw} is not a valid number");
+        continue;
+    }
+}
 //ask user which category to display, hometown or favorite food
 
 //display relevant info
