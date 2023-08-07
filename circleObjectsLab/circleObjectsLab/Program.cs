a48f2eab40f55754647e48802fb18a978622c6e6
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2023-08-07 17:55:49 -0400

    psuedocode outlined

---
+++ circleObjectsLab/circleObjectsLab/Program.cs
@@ -0,0 +1,14 @@
+﻿//circle objects lab
+
+//prompt user to enter a radius, may contain decimals(use a double)
+
+//displays an error if user inputs invalid data, and asks again
+
+//when valid data entered, create instance of circle, then uses methods to display diameter, circumfrence and area
+
+//asks user if circle should grow
+
+//if yes, call grow method and display diameter, circumfrence and area
+//grow method should adjust radius to double its value
+
+//if user says no, display "goodbye" and the radius of the circle
